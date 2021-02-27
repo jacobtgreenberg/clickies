8f862f94715cfb5ad5eda58c418e2028dcdf7685
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-27 14:59:05 -0500

    succesfully created user

--- third/controllers/userscont.js
+++ third/controllers/userscont.js
@@ -1,11 +1,19 @@
 const bcrypt = require('bcrypt')
 const express = require('express')
 const users = express.Router()
-const User = require('../models/users.js')
+const User = require('../models/usersmodel.js')
 
 
 users.get('/', (req, res) => {
-    res.send('hello there')
+    res.render('users.ejs')
+})
+
+users.post('/' ,(req, res)=> {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+    User.create(req.body, (err, createdUser) => {
+        console.log(createdUser)
+        res.redirect('/')
+    })
 })
 
 module.exports = users
