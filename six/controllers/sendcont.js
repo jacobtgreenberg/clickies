25ae67007cba5e3a6cc237e2eaf34458a3739ea1
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-28 20:26:04 -0500

    more sending logic involving tags

--- six/controllers/sendcont.js
+++ six/controllers/sendcont.js
@@ -24,14 +24,19 @@ send.post('/upsend/:id',(req, res) => {
 })
 
 send.post('/commit',(req, res) => {
+    console.log(req.body.tags.length)
     User.findOne({username: req.body.to}, (error, foundUser)=>{
         if(foundUser.username === undefined){
             res.send('no such person')
         }else if(req.body.to === 'PUBLIC'){
             req.body.user = req.body.to
-            req.body.tags += ',PUBLIC'
-            req.body.tags = req.body.tags.split(",")
-            req.body.tags = req.body.tags.map(s => s.trim())
+            if(req.body.tags.length === 0){
+                req.body.tags = 'PUBLIC'
+            }else{
+                req.body.tags += ',PUBLIC'
+                req.body.tags = req.body.tags.split(",")
+                req.body.tags = req.body.tags.map(s => s.trim())
+            }
             Clicky.create(req.body, (err, newClick) =>{
                 req.body.user = req.session.currentUser
                 Clicky.create(req.body, (err, anotherClick)=> {
@@ -41,12 +46,21 @@ send.post('/commit',(req, res) => {
         }else{
              req.body.user = req.body.to
              req.body.inbox = true
-             req.body.tags += `,to:${req.body.to},from:${req.session.currentUser}`
-             req.body.tags = req.body.tags.split(",")
-             req.body.tags = req.body.tags.map(s => s.trim())
+             if(req.body.tags.length === 0){
+                 req.body.tags = `from:${req.session.currentUser}`
+             }else{
+                req.body.tags += `,from:${req.session.currentUser}`
+                req.body.tags = req.body.tags.split(",")
+                req.body.tags = req.body.tags.map(s => s.trim())
+             }
              Clicky.create(req.body, (err, newClick) =>{
                 req.body.user = req.session.currentUser
                 req.body.inbox = false
+                if(req.body.tags === `from:${req.session.currentUser}`){
+                    req.body.tags = `to:${req.body.to}`
+                }else{
+                    req.body.tags.splice(req.body.tags.length - 1, 1 , `to:${req.body.to}`)
+                }
                 Clicky.create(req.body, (err, anotherClick) =>{
                     res.redirect('/')
                 })
@@ -56,29 +70,43 @@ send.post('/commit',(req, res) => {
 })
 
 send.post('/upcommit/:id',(req, res) => {
-    console.log(req.params.id)
     User.findOne({username: req.body.to}, (error, foundUser)=>{
         console.log(req.body)
         if(foundUser.username === undefined){
             res.send('no such person')
         }else if(req.body.to === 'PUBLIC'){
             req.body.user = req.body.to
+            if(req.body.tags.length === 0){
+                req.body.tags = 'PUBLIC'
+            }else{
             req.body.tags += ',PUBLIC'
             req.body.tags = req.body.tags.split(",")
             req.body.tags = req.body.tags.map(s => s.trim())
+            }
             Clicky.create(req.body, (err, newClick) =>{
                 req.body.user = req.session.currentUser
-                Clicky.create(req.body, (err, anotherClick)=> {
+                Clicky.findByIdAndUpdate(req.params.id, {tags: req.body.tags} , (err, anotherClick)=> {
                     res.redirect('/')
                 })
             })   
         }else{
              req.body.user = req.body.to
              req.body.inbox = true
-             req.body.tags += `,to:${req.body.to},from:${req.session.currentUser}`
-             req.body.tags = req.body.tags.split(",")
-             req.body.tags = req.body.tags.map(s => s.trim())
+             if(req.body.tags.length === 0){
+                 req.body.tags =`from:${req.session.currentUser}`
+             }else{
+                req.body.tags += `,from:${req.session.currentUser}`
+                req.body.tags = req.body.tags.split(",")
+                req.body.tags = req.body.tags.map(s => s.trim())
+             }
              Clicky.create(req.body, (err, newClick) =>{
+                if(req.body.tags === `from:${req.session.currentUser}`){
+                    req.body.tags = `to:${req.body.to}`
+                }else{
+                    console.log(req.body.tags.length)
+                    console.log(req.body.tags)
+                    req.body.tags.splice(req.body.tags.length - 1, 1 , `to:${req.body.to}`)
+                }
                 Clicky.findByIdAndUpdate(req.params.id, {tags: req.body.tags} , (err, anotherClick) =>{
                     res.redirect('/')
                 })
